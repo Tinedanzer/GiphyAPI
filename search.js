95c7230f5eb59e2769b871407922d0f3541ba345
Tinedanzer <johnhillallen@gmail.com>
2018-07-21 00:35:33 -0500

    ladida

--- search.js
+++ search.js
@@ -1,30 +1,48 @@
 var topics= ["Cheer Bear","Grumpy Bear","Share Bear","Funshine Bear","Tenderheart Bear"];
-
+var counter=0;
 for (let i = 0; i < topics.length; i++) 
 {
  var topic1= topics[i];
- 
- var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ru1FqB2awevSCsIvdrTZ3Q2K4kJdihkX&q=" + topics[i] + "&limit=10&offset=0&lang=en";
+ var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=ru1FqB2awevSCsIvdrTZ3Q2K4kJdihkX&q=" + topics[i] + "&limit=10&offset=0&lang=en";
     $.ajax({
        url: queryURL,
         method: "GET"
-       }).then(function(response) {
+       }).then(function(response) 
+  {
          console.log(response);
-        //  var newbutton= document.getElementsByClassName('Aroo')+i;
+        //  id=button${topics[i]}
+         //  var newbutton= document.getElementsByClassName('Aroo')+i;
          $(".Aroo").append(`
-         <button id=button${topics[i]} value=button>${topics[i]}
+         <button value=button>${topics[i]}
          </button>`);
+         $("button").attr('id', x => `button${x}`);
+         
+        //  $(".Aroo").each(function(){
+        //    $(this).attr("id", "button" + i);
+        //    console.log(this);
+        //    counter++;
+        //    console.log(counter);  
+        //  })
         //  $(".Aroo").attr(id, buttoni);
-        $("button").on("click", function () 
+        $("#button0").on("click", function () 
       {
                  
          $("#friends").prepend(`
-         <div>${response.data[i].title}</div>
-         <div><img src="${response.data[i].images.fixed_width_still.url}"></div>
-         <div>${response.data[i].rating}</div>
-         `);
+         <div>${response.data[0].title}</div>
+         <div><img src="${response.data[0].images.fixed_width_still.url}"></div>
+         <div>${response.data[0].rating}</div>
+         `);  
        });
-      });
+       $("#button1").on("click", function () 
+      {
+                 
+         $("#friends").prepend(`
+         <div>${response.data[1].title}</div>
+         <div><img src="${response.data[1].images.fixed_width_still.url}"></div>
+         <div>${response.data[1].rating}</div>
+         `);  
+       });
+  });
 };
 
 
