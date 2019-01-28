cfb9f841bd62d7849ef3cfc35e3cc482648bff88
Tinedanzer <johnhillallen@gmail.com>
2019-01-28 02:21:12 -0600

    rearranging code,  minor updates

--- search.js
+++ search.js
@@ -1,48 +1,57 @@
-$(document).ready(function(){
-var topics= ["Cheer Bear","Grumpy Bear","Share Bear","Funshine Bear","Tenderheart Bear"];
-
+const topics= ["Cheer Carebear","Grumpy Carebear","Share Bear","Funshine Bear","Tenderheart Bear"];
+let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ru1FqB2awevSCsIvdrTZ3Q2K4kJdihkX&q=" + "carebears" + "&limit=10&offset=0&lang=en";
 for (let i = 0; i < topics.length; i++) 
 {
- var topics1= topics[i];
-//  var newarray = topics.map(function(item, index){
- var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ru1FqB2awevSCsIvdrTZ3Q2K4kJdihkX&q=" + topics + "&limit=10&offset=0&lang=en";
+ let topics1= topics[i];
+//  const newarray = topics.map(function(item, index){
+  // ****fixed topics by adding an [i]****************
+//  let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ru1FqB2awevSCsIvdrTZ3Q2K4kJdihkX&q=" + "carebears" + "&limit=10&offset=0&lang=en";
     $.ajax({
        url: queryURL,
         method: "GET"
        }).then(function(response) 
   {
          console.log(response);
-//          var newbutton= $("<button>");
+
+         $(".Aroo").append(`<button value=button>${topics[i]}</button>`);
+         $("button").attr('id', i => `button${i}`)
+  });
+};
+// *************************************************************************************************************
+// ************TAKE THIS BUTTON CLICK OUT OF THe FOR LOOP, MAKE ANOTHER FOR LOOP WITHIN THE BUTTON**************
+// *************************************************************************************************************
+$.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) 
+    {
+      $("button").on("click", function () {
+        for (let i = 0; i < 5; i++) {
+          let chilly= response.data[i].images.fixed_width_still.url;
+           $("#friends").prepend(`
+           <div>${response.data[i].title}</div>
+          <div><img src="${chilly}"></div>
+           <div>${response.data[i].rating}</div>
+           `); 
+        }
+       });
+     });
+//          const newbutton= $("<button>");
 //          newbutton.attr('id', `button${index}`);
 //          newbutton.text(item);
-//         var traits= $('<img/>');
+//         const traits= $('<img/>');
 //         traits.attr('src', response.data[index].images.fixed_width_still.url);
 //         traits.attr('data-still', response.data[index].images.fixed_width_still.url);
 //         traits.attr('data-animate', response.data[index].images.fixed_width.url);
         
-//         var names =`<div>${response.data[index].title}</div>`;
+//         const names =`<div>${response.data[index].title}</div>`;
 //         console.log(names);
-//         var ratings= `<div>${response.data[index].rating}</div>`;
+//         const ratings= `<div>${response.data[index].rating}</div>`;
 //          newbutton.click(function(){
 //           $("#friends").prepend(traits, names, ratings); 
          
 //          });
-        //  $(".Aroo").append(newbutton);
-         $(".Aroo").append(`
-         <button value=button>${topics[i]}
-         </button>`);
-         $("button").attr('id', x => `button${x}`)
-
-        var chilly= response.data[i].images.fixed_width_still.url;
-        
-        $("button").on("click", function () 
-      {
-         $("#friends").prepend(`
-         <div>${response.data[i].title}</div>
-         <div><img src="${chilly}"></div>
-         <div>${response.data[i].rating}</div>
-         `);  
-       });
+  //  $(".Aroo").append(newbutton);
     //    $("#button1").on("click", function () 
     //   {                 
     //      $("#friends").prepend(`
@@ -75,9 +84,4 @@ for (let i = 0; i < topics.length; i++)
     //      <div>${response.data[4].rating}</div>
     //      `);  
     //    });
-  });
-
-
 // });
-};
-})
